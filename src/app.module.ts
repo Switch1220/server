66b71d2ce04fb805c2db7bac4b5007292a637b0b
Mideum Han <switchanmd@gmail.com>
2022-02-14 02:07:35 +0800

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from '@modules/events/events.module';
 import { VpnModule } from './modules/vpn/vpn.module';
 import { PrismaModule } from './providers/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [PrismaModule, VpnModule, EventsModule],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    PrismaModule,
+    VpnModule,
+    EventsModule,
+  ],
 })
 export class AppModule {}

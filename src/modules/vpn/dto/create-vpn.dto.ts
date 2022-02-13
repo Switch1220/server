d6d649d729d59c1e47251e47cb7609104a538ef2
Mideum Han <switchanmd@gmail.com>
2022-02-13 18:11:21 +0800

    feat: add validation to create vpn dto

--- src/modules/vpn/dto/create-vpn.dto.ts
+++ src/modules/vpn/dto/create-vpn.dto.ts
@@ -1,6 +1,15 @@
+import { IsString } from 'class-validator';
+
 export class CreateVpnDto {
+  @IsString()
   readonly vpnAddress: string;
+
+  @IsString()
   readonly vpnName: string;
+
+  @IsString()
   readonly vpnUsername: string;
+
+  @IsString()
   readonly vpnPassword: string;
 }

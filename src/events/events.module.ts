9e47525441cf0d52e341e0c182a6417a066fa613
Mideum Han <switchanmd@gmail.com>
2022-02-13 17:29:29 +0800

    chore: change paths

--- src/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
-import { EventsGateway } from './events.gateway';
-
-@Module({
-  imports: [ConfigModule],
-  providers: [EventsGateway],
-})
-export class EventsModule {}
